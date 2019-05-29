826e31558ac9e57737f45756252cd5cff2d6cb99
暴力兔 <oncwnuKifZqCFE1r7bZt5Jh1nPeo@git.weixin.qq.com>
2019-05-29 17:03:03 +0800

    更换深色背景，可更换楼层

--- js/main.js
+++ js/main.js
@@ -1,40 +1,65 @@
-window.onload=function(){
-  var div = document.getElementById('mapContainer');
+var strFloor = "floor";
+var strColor = "color";
+var div;
+var projection;
+var notinited = true;
+var init = function () {
+  div = document.getElementById('mapContainer');
+  window.selectFloorOnChange = function(floor){
+    refreshTheMap(floor.value);
+  };
+  refreshTheMap(1);
+  console.log(floorIndices);
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showUserPosition, function (e) {
+      console.log(e.code);
+    });
+  }
+  notinited = false;
+}
+var refreshTheMap = function (floorIdx) {
+  currentFloor = Number(floorIdx);
   var width = div.clientWidth;
   var height = div.clientHeight;
-  var projection = d3.geoMercator()
-    .center([116.356666,39.7592])
+  projection = d3.geoMercator()
+    .center([116.356666, 39.7592])
     .scale(10000000)
-    .translate([width/2, height/2]);
+    .translate([width / 2, height / 2]);
   var path = d3.geoPath()
-    .projection(projection);s
-  var svg = d3.select("#mapContainer").append("svg")
+    .projection(projection);
+  var svg = d3.select("svg")
     .attr("width", width)
-    .attr("height", height)
-  d3.json("doc/geoJsonPKU.json", function(error, json){
-    if (error) console.log(error);
+    .attr("height", height);
+  svg.selectAll("path").remove();
+  d3.json("doc/geoJsonPKU.json", function (error, json) {
     svg.selectAll("path")
       .data(json.features)
       .enter()
       .append("path")
       .attr("d", path)
-      .style("fill", "steelblue");
+      .style("visibility", function (d) {
+        var floorIdx =Number(d.properties.floor);
+        var contained = false;
+        floorIndices.forEach(function(idx)
+        {if(idx == floorIdx)contained =true;})
+        if(contained===false) {
+          var floorSelectDom = document.getElementById("floorSelect");
+          floorSelectDom.options.add(new Option(d.properties.floor,floorIdx));
+        }floorIndices.push(floorIdx);
+        console.log(typeof currentFloor);
+        console.log(currentFloor);
+        console.log(typeof floorIdx);
+        console.log(floorIdx);
+        return floorIdx === currentFloor? "visible" : "hidden";
+      })
   });
-
-  function showUserPosition(position)
-  {
-    var geoPoint = [];
-    geoPoint.push(position.coords.longitude);
-    geoPoint.push(position.coords.latitude);
-    alert(geoPoint + "" + projection(geoPoint));
-    d3.select("#userPoint")
-      .style("left", ""+projection(geoPoint)[0]+"px")
-      .style("top", ""+projection(geoPoint)[1]+"px")
-  }
-  if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showUserPosition, function(e)
-    {
-      alert(e.code);
-    });
-  }
+};
+function showUserPosition(position) {
+  var geoPoint = [];
+  geoPoint.push(position.coords.longitude);
+  geoPoint.push(position.coords.latitude);
+  console.log(geoPoint + "" + projection(geoPoint));
+  d3.select("#userPoint")
+    .style("left", "" + projection(geoPoint)[0] + "px")
+    .style("top", "" + projection(geoPoint)[1] + "px")
 }
